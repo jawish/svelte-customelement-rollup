fcb0c1811715a3fcbf0b702a64e743cd7b93537f
Jawish Hameed <jawish@gmail.com>
2020-05-10 19:11:53 +0800

    Initial commit

---
+++ src/NestedComponent.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let color = '#ffcc00';
+
+  $: avatarColor = color.substring(1);
+</script>
+
+<img src={`https://api.adorable.io/avatars/face/eyes4/nose3/mouth7/${avatarColor}`} alt="Avatar" width="150" />
